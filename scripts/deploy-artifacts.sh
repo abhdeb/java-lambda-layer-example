0da21650d0ff3dad3bc319378b2c3977d822e81b
Abhra Prakash Deb <abhra.dev@gmail.com>
2020-12-01 15:37:26 +0530

    Updated template file name

--- scripts/deploy-artifacts.sh
+++ scripts/deploy-artifacts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ARTIFACT_BUCKET=$(cat bucket-name.txt)
-TEMPLATE=sam-template.yml
+TEMPLATE=template.yml
 aws cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
 aws cloudformation deploy --template-file out.yml --stack-name java-lambda-layer-example --capabilities CAPABILITY_NAMED_IAM
